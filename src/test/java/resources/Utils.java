dc6d8a850b47e528f3a0d8385fc8bcea790eee52
pardesidinesh <pardesi.dinesh@yahoo.co.uk>
2020-02-12 11:23:33 +0000

    UPdated the utils
    Minor update to fileinputstream path

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -26,7 +26,7 @@ public class Utils {
 
     public static String getGlobalValue(String key) throws IOException {
         Properties prop = new Properties();
-        FileInputStream fis = new FileInputStream("E:\\APIFramework\\APIFramework\\src\\test\\java\\resources\\global.properties");
+        FileInputStream fis = new FileInputStream("src/test/java/resources/global.properties");
         prop.load(fis);
         return prop.getProperty(key);
 
